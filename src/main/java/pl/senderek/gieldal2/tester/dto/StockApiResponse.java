fb9d0f6ad054eecc4026d2728bc768b0b9c2d619
Marcin Paul <megamen1285@gmail.com>
2019-11-21 00:51:31 +0100

    Aktualizacja StockApiResponse o nowe pola
    Nie wiem czy są nam one potrzebne, ale backend takie zwraca

--- src/main/java/pl/senderek/gieldal2/tester/dto/StockApiResponse.java
+++ src/main/java/pl/senderek/gieldal2/tester/dto/StockApiResponse.java
@@ -2,10 +2,31 @@ package pl.senderek.gieldal2.tester.dto;
 
 public class StockApiResponse {
     private Integer backendTime;
+
     private Integer selectsTime;
     private Integer selectsCount;
     private Integer updatesTime;
     private Integer updatesCount;
+    private Integer insertsTime;
+    private Integer insertsCount;
+    private Integer deletesTime;
+    private Integer deletesCount;
+
+    public Integer getInsertsTime() { return insertsTime; }
+
+    public void setInsertsTime(Integer insertsTime) { this.insertsTime = insertsTime; }
+
+    public Integer getInsertsCount() { return insertsCount; }
+
+    public void setInsertsCount(Integer insertsCount) { this.insertsCount = insertsCount; }
+
+    public Integer getDeletesTime() { return deletesTime; }
+
+    public void setDeletesTime(Integer deletesTime) { this.deletesTime = deletesTime; }
+
+    public Integer getDeletesCount() { return deletesCount; }
+
+    public void setDeletesCount(Integer deletesCount) { this.deletesCount = deletesCount; }
 
     public Integer getBackendTime() {
         return backendTime;
